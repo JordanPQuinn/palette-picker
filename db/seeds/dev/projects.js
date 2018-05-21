86656116d6c62188e98901305325a955654d5d66
Jordan Quinn <jpquinn605@gmail.com>
2018-05-21 16:15:17 -0600

    setup backend routes

--- db/seeds/dev/projects.js
+++ db/seeds/dev/projects.js
@@ -1,20 +1,23 @@
 
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('projects').del()
+  return knex('project').del()
     .then(function () {
       // Inserts seed entries
       return Promise.all([
-        knex('projects').insert({
+        knex('project').insert({
           name: 'test-project'}, 'id')
         .then(project => {
-          return knex('palettes').insert([
+          return Promise.all([
+            knex('palettes').insert([
             {
               name: 'sample-palette',
               project_id: project[0],
               colors: ['#B38CB4', '#B7918C', '#C5A48A', '#DDC67B', '#F8F272']
             }
           ])
+        ]);
         })
         .then(() => console.log('seeding complete!'))
         .catch(error => console.log(`${error}`))
