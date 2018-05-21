ec1277f03fb0be70e1c3a391b78c8f6465cc1e1f
Jordan Quinn <jpquinn605@gmail.com>
2018-05-21 13:47:21 -0600

    initial fetch calls

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,33 @@
 const fetchButton = document.querySelector('.fetch-button');
-const colorBoxOne = document.getElementById('color-one');
+
 
 const getStuff = async () => {
-  var response = await fetch('/get');
-  var parsedResponse = await response.json();
-  colorBoxOne.style.backgroundColor = `${parsedResponse}`;
+  let response = await fetch('/api/v1/projects');
+  let parsedResponse = await response.json();
+  console.log(parsedResponse);
 }
 
-fetchButton.addEventListener('click', getStuff);
+const generateRandomColor = () => {
+  const hexValues = '0123456789ABCDEF';
+  let hexString = '#'
+  for(let i = 0; i < 6; i++) {
+      let letter = hexValues[Math.floor(Math.random() * 16)];
+      hexString += letter;
+  }
+  return hexString;
+}
 
+const createPaletteValues = (colorBoxes) => {
+  colorBoxes.forEach(box => {
+    box.style.backgroundColor = generateRandomColor();
+  });
+}
+
+const renderPaletteBoxes = () => {
+  const colorBoxes = document.querySelectorAll('.color-box');
+  createPaletteValues(colorBoxes);
+}
+
+fetchButton.addEventListener('click', renderPaletteBoxes);
+
+window.addEventListener('load', getStuff);
--- server.js
+++ server.js
@@ -2,15 +2,21 @@ const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 app.locals.title = 'Palette Picker';
-app.locals.colorsArray = ['', '', '', '', '']
 
 app.use(express.static('public'));
 
-app.get('/get', (request,response) => {
-  var body = 'black';
-  return response.status(200).json(body);
-
+app.get('/api/v1/projects', (request,response) => {
+  database('projects').select()
+    .then((projects) => {
+        return response.status(200).json(projects);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
 });
 
 app.listen(app.get('port'), () => {
